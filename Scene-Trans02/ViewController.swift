db47670c50c848eb699a7a3a6305a73c8121982b
윤성호 <youn2029@naver.com>
2019-01-10 00:21:29 +0900

    Initial Commit

---
+++ Scene-Trans02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scene-Trans02
+//
+//  Created by 윤성호 on 2019. 1. 10..
+//  Copyright © 2019년 윤성호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
