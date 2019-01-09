f69e7b912118ddbb6296efa5d0ed27e5b90ac265
윤성호 <youn2029@naver.com>
2019-01-10 00:52:12 +0900

    내비게이션 컨트롤러 추가 방법 및 화면 전환 구현 실습

--- Scene-Trans02/ViewController.swift
+++ Scene-Trans02/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // bar button item 클릭 이벤트
+    @IBAction func moveByNavi(_ sender: Any) {
+        
+        // 두번째 뷰 컨트롤러 인스턴스를 가져온다
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") else {
+            
+            return
+        }
+        
+        // 화면을 전환한다.
+        self.navigationController?.pushViewController(uvc, animated: true)
+    }
+    
 }
 
